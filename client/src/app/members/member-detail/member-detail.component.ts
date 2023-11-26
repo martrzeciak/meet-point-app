94bd5bcd4ad8f614adf519fd1d5acab4ae37104b
Mariusz Trzeciak <mariusz.trzeciak09@gmail.com>
2023-11-26 18:45:59 +0100

    Pre angular 16

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,10 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
+  standalone: true,
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  imports: [CommonModule, TabsModule, GalleryModule]
 })
 export class MemberDetailComponent {
+  member: Member | undefined;
 
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    var username = this.route.snapshot.paramMap.get('username');
+    if (username == null) return;
+    this.memberService.getMember(username).subscribe({
+      next: member => {
+        this.member = member;
+      }
+    })
+  }
 }
