bf62223ccf54e1685313acd183f963be54caeb01
martrzeciak <mariusz.trzeciak09@gmail.com>
2023-12-02 21:08:37 +0100

    Add validators, expand register form, update API register method

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, Form, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,25 +11,67 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  registerForm: FormGroup = new FormGroup({});
+  maxDate: Date = new Date();
+  validationErrors: string[] | undefined;
   
-  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+  constructor(private accountService: AccountService, private toastr: ToastrService,
+    private formBuilder: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
-    
+    this.initalizeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  initalizeForm() {
+    this.registerForm = this.formBuilder.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', 
+        [Validators.required, 
+        Validators.minLength(4), 
+        Validators.maxLength(8)]],
+      confirmPassword: ['', 
+        [Validators.required,
+        this.matchValues('password')]]
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.get(matchTo)?.value ? null : {isMatching: true}
+    }
   }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: _ =>  this.cancel(),
+    const date = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
+    const values = {...this.registerForm.value, dateOfBirth: date};
+    this.accountService.register(values).subscribe({
+      next: _ =>  {
+        this.router.navigateByUrl('/members')
+      },
       error: error => {
-        this.toastr.error(error.error),
-        console.log(error);
+        this.validationErrors = error;
       }
     })
   }
 
   cancel() {
     this.cancelRegister.emit(false);
+  }
+
+  private getDateOnly(date: string | undefined) {
+    if (!date) return;
+    let theDate = new Date(date);
+    return new Date(theDate.setMinutes(theDate.getMinutes() - theDate.getTimezoneOffset()))
+      .toISOString()
+      .slice(0, 10);
   }
 }
